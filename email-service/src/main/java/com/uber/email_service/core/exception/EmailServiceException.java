aa0852e85dc0d1ef966ba01beb9e7ce1c6690c47
rafaelnapolitano <rafaelnv.prof@gmail.com>
2024-08-26 15:01:53 -0300

    feat: create EmailServiceException

---
+++ email-service/src/main/java/com/uber/email_service/core/exception/EmailServiceException.java
@@ -0,0 +1,12 @@
+package com.uber.email_service.core.exception;
+
+public class EmailServiceException extends RuntimeException{
+
+    public EmailServiceException(String message) {
+        super(message);
+    }
+
+    public EmailServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
