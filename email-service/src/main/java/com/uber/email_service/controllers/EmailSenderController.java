0ec50ff8c3e00892a1773f2680b5f7cff5d21abb
rafaelnapolitano <rafaelnv.prof@gmail.com>
2024-08-26 15:03:04 -0300

    feat: create SesEmailSender, EmailSender, EmailSenderGateway, EmailSenderService, EmailSenderUseCase

---
+++ email-service/src/main/java/com/uber/email_service/controllers/EmailSenderController.java
@@ -0,0 +1,34 @@
+package com.uber.email_service.controllers;
+
+import com.uber.email_service.application.EmailSenderService;
+import com.uber.email_service.core.EmailRequest;
+import com.uber.email_service.core.exception.EmailServiceException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/email")
+public class EmailSenderController {
+
+    private final EmailSenderService emailSenderService;
+
+    @Autowired
+    public EmailSenderController(EmailSenderService emailSenderService) {
+        this.emailSenderService = emailSenderService;
+    };
+
+    @PostMapping()
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequest    request) {
+        try {
+            this.emailSenderService.sendEmail(request.to(), request.subject(), request.body());
+            return ResponseEntity.ok("email send successfully");
+        } catch (EmailServiceException ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while sending email");
+    }}
+}
---
+++ email-service/src/main/java/com/uber/email_service/core/EmailSenderUseCase.java
@@ -0,0 +1,5 @@
+package com.uber.email_service.core;
+
+public interface EmailSenderUseCase {
+    void sendEmail(String to, String subject, String body);
+}
